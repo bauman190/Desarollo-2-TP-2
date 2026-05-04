62b44f09e227810066775b0d6584a5fa99ff1209
Bauman <baumancastignani@hotmail.com>
2026-05-04 11:58:45 -0300

    Credits panel added to main menu (empty, only back button)

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -6,26 +6,34 @@ public class UIMainMenu : MonoBehaviour
 {
     [SerializeField] private Button buttonPlay;
     [SerializeField] private Button buttonSettings;
+    [SerializeField] private Button buttonCredits;
     [SerializeField] private Button buttonExit;
-    [SerializeField] private Button buttonBack;
+    [SerializeField] private Button buttonSettingsBack;
+    [SerializeField] private Button buttonCreditsBack;
 
     [SerializeField] private CanvasGroup panelMain;
     [SerializeField] private CanvasGroup panelSettings;
+    [SerializeField] private CanvasGroup panelCredits;
 
     private void Awake()
     {
         buttonPlay.onClick.AddListener(OnButtonPlayClicked);
         buttonSettings.onClick.AddListener(OnButtonSettingsClicked);
+        buttonCredits.onClick.AddListener(OnButtonCreditsClicked);
         buttonExit.onClick.AddListener(OnButtonExitClicked);
-        buttonBack.onClick.AddListener(OnButtonBackClicked);
+        buttonSettingsBack.onClick.AddListener(OnButtonSettingsBackClicked);
+        buttonCreditsBack.onClick.AddListener(OnButtonCreditsBackClicked);
     }
 
     private void OnDestroy()
     {
         buttonPlay.onClick.RemoveAllListeners();
         buttonSettings.onClick.RemoveAllListeners();
+        buttonCredits.onClick.RemoveAllListeners();
         buttonExit.onClick.RemoveAllListeners();
-        buttonBack.onClick.RemoveAllListeners();
+        buttonSettingsBack.onClick.RemoveAllListeners();
+        buttonCreditsBack.onClick.RemoveAllListeners();
+
     }
     private void OnButtonPlayClicked()
     {
@@ -38,11 +46,23 @@ public class UIMainMenu : MonoBehaviour
         SetPanel(panelMain, false);
         SetPanel(panelSettings, true);
     }
-   
-    private void OnButtonBackClicked()
+
+    private void OnButtonCreditsClicked()
+    {
+        SetPanel(panelMain, false);
+        SetPanel(panelCredits, true);
+    }
+
+    private void OnButtonSettingsBackClicked()
     {
         SetPanel(panelMain, true);
         SetPanel(panelSettings, false);
+    }
+
+    private void OnButtonCreditsBackClicked()
+    {
+        SetPanel(panelMain, true);
+        SetPanel(panelCredits, false);
     }
     private void SetPanel(CanvasGroup panel, bool state)
     {
