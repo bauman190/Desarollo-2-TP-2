31179d513cbf9372c781157c6fd14dff128e223d
Bauman <baumancastignani@hotmail.com>
2026-05-04 23:36:07 -0300

    Better pause menu, can resume, change settings and go back to main menu

---
+++ Assets/Scripts/UI/UIPause.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class UIPause : MonoBehaviour
+{
+    [SerializeField] private Button buttonResume;
+    [SerializeField] private Button buttonSettings;
+    [SerializeField] private Button buttonMainMenu;
+
+    [SerializeField] private Button buttonSettingsBack;
+
+    [SerializeField] private CanvasGroup panelPause;
+    [SerializeField] private CanvasGroup panelSettings;
+
+    private bool isPause = false;
+
+    private void Awake()
+    {
+        buttonResume.onClick.AddListener(OnButtonResumeClicked);
+        buttonSettings.onClick.AddListener(OnButtonSettingsClicked);
+        buttonMainMenu.onClick.AddListener(OnButtonMainMenuClicked);
+        buttonSettingsBack.onClick.AddListener(OnButtonSettingsBackClicked);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        buttonResume.onClick.RemoveAllListeners();
+        buttonSettings.onClick.RemoveAllListeners();
+        buttonMainMenu.onClick.RemoveAllListeners();
+    }
+
+    private void OnButtonResumeClicked()
+    {
+        TogglePause();
+    }
+    private void OnButtonSettingsClicked()
+    {
+        SetPanel(panelPause, false);
+        SetPanel(panelSettings, true);
+    }
+    private void OnButtonMainMenuClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    private void OnButtonSettingsBackClicked()
+    {
+        SetPanel(panelPause, true);
+        SetPanel(panelSettings, false);
+    }
+
+    private void SetPanel(CanvasGroup panel, bool state)
+    {
+        panel.alpha = state ? 1 : 0;
+        panel.interactable = state;
+        panel.blocksRaycasts = state;
+    }
+    void TogglePause()
+    {
+        isPause = !isPause;
+        Time.timeScale = isPause ? 0 : 1;
+        SetPanel(panelPause, isPause);
+        Cursor.lockState = isPause ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = isPause;
+    }
+}
